5a70f18d5b267320fc44ce734e0dd85c0ab83dad
Mark S. Bentley <mark@lunartech.org>
2019-10-24 15:50:15 +0200

    Bumped version number for recent changes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mustlink',
-      version='0.1',
+      version='0.2',
       description='MUSTlink API wraper',
       author='Mark S. Bentley',
       author_email='mark@lunartech.org',
