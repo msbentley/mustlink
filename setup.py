e847f917c8665844439760374f7c06325edafc4f
Mark S. Bentley <mark@lunartech.org>
2020-10-21 13:54:58 +0200

    Added more meta-data for packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
 setup(name='mustlink',
-      version='0.3',
-      description='MUSTlink API wraper',
-      author='Mark S. Bentley',
-      author_email='mark@lunartech.org',
-      packages=['mustlink'],
-      zip_safe=False)
-
+    version='0.3',
+    description='MUSTlink API wraper',
+    author='Mark S. Bentley',
+    author_email='mark@lunartech.org',
+    description='A python wrapper for the ESA mustlink API'
+    long_description_content_type="text/markdown",
+    url="https://github.com/msbentley/mustlink",
+    download_url = 'https://github.com/msbentley/mustlink/archive/v0.3.tar.gz'
+    python_requires='>=3.0',
+    keywords = ['telemetry', 'MUST', 'ESA'],
+    packages=['mustlink'],
+    zip_safe=False)
