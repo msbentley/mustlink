fb45bb8aafc540d9220034b4024cddbf58ba4c21
Mark S. Bentley <mark@lunartech.org>
2020-10-21 15:40:13 +0200

    Preparing packaging for 0.3.1 release

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='mustlink',
-    version='0.3',
+    version='0.3.1a1',
     author='Mark S. Bentley',
     author_email='mark@lunartech.org',
     description='A python wrapper for the ESA mustlink API',
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/msbentley/mustlink",
-    download_url = 'https://github.com/msbentley/mustlink/archive/v0.3.tar.gz',
+    download_url = 'https://github.com/msbentley/mustlink/archive/0.3.1.tar.gz',
+    install_requires=['matplotlib','pandas','pyyaml','requests'],
     python_requires='>=3.0',
     keywords = ['telemetry', 'MUST', 'ESA'],
     packages=['mustlink'],
