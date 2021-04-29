4fc130a74c0fae0f3698e424e4600faab551b733
Mark S. Bentley <mark@lunartech.org>
2021-04-29 13:22:56 +0200

    Added pysocks dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='mustlink',
     long_description_content_type="text/markdown",
     url="https://github.com/msbentley/mustlink",
     download_url = 'https://github.com/msbentley/mustlink/archive/0.3.1.tar.gz',
-    install_requires=['matplotlib','pandas','pyyaml','requests'],
+    install_requires=['matplotlib','pandas','pyyaml','requests','pysocks'],
     python_requires='>=3.0',
     keywords = ['telemetry', 'MUST', 'ESA'],
     zip_safe=False)
