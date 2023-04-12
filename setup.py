51f331e14df1e819e472f40783e7664d5b7dd65b
Mark S. Bentley <mark.bentley@esa.int>
2023-04-12 09:27:47 +0200

    Bumped version to 0.3.2 for point release

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='mustlink',
-    version='0.3.1',
+    version='0.3.2',
     author='Mark S. Bentley',
     author_email='mark@lunartech.org',
     description='A python wrapper for the ESA mustlink API',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/msbentley/mustlink",
-    download_url = 'https://github.com/msbentley/mustlink/archive/0.3.1.tar.gz',
+    download_url = 'https://github.com/msbentley/mustlink/archive/0.3.2.tar.gz',
     install_requires=['matplotlib','pandas','pyyaml','requests','pysocks'],
     python_requires='>=3.0',
     keywords = ['telemetry', 'MUST', 'ESA'],
